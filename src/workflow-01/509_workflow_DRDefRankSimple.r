15701bed2a62a3117c3f0a249e1375dc2fcb2b40
Juan Manuel Truffa <jtruffa@itba.edu.ar>
2024-05-31 14:35:29 +0000

    Ajuste de envíos 9500 12500

--- src/workflow-01/509_workflow_DRDefRankSimple.r
+++ src/workflow-01/509_workflow_DRDefRankSimple.r
@@ -362,8 +362,8 @@ ZZ_final_base9 <- function( pinputexps )
   param_local$train$gan0 <-  -3000
   param_local$train$meseta <- 2001
   
-  param_local$kaggle$envios_desde <-  8000L
-  param_local$kaggle$envios_hasta <- 14000L
+  param_local$kaggle$envios_desde <-  9500
+  param_local$kaggle$envios_hasta <- 12500
   param_local$kaggle$envios_salto <-   500L
   param_local$kaggle$competition <- "itba-data-mining-2024-a"
   
