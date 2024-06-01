94991f983ea0739c6574a6e37670d9af7fac81fe
Juan Manuel Truffa <jtruffa@itba.edu.ar>
2024-06-01 03:12:36 +0000

    541 al estado original

--- src/workflow-01/z541_DR_corregir_drifting.r
+++ src/workflow-01/z541_DR_corregir_drifting.r
@@ -138,15 +138,13 @@ campos_monetarios <- colnames(dataset)
 campos_monetarios <- campos_monetarios[campos_monetarios %like%
   "^(m|Visa_m|Master_m|vm_m)"]
 
-camposVar3 = colnames(dataset)
-camposVar3 = camposVar3[camposVar3 %like% "^c" & !camposVar3 %like% "clase_ternaria"]
 
 # aqui aplico un metodo para atacar el data drifting
 # hay que probar experimentalmente cual funciona mejor
 switch(envg$PARAM$metodo,
   "ninguno"        = cat("No hay correccion del data drifting"),
   "rank_simple"    = drift_rank_simple(campos_monetarios),
-  "rank_cero_fijo" = drift_rank_cero_fijo(camposVar3),
+  "rank_cero_fijo" = drift_rank_cero_fijo(campos_monetarios),
   "deflacion"      = drift_deflacion(campos_monetarios),
   "estandarizar"   = drift_estandarizar(campos_monetarios)
 )
