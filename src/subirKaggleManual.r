539d3380504ba5ebedc269ade99a28db635348c5
Juan Manuel Truffa <jtruffa@itba.edu.ar>
2024-06-02 22:50:20 +0000

    601 Semillerio Deflacion+RankFijo

--- src/subirKaggleManual.r
+++ src/subirKaggleManual.r
@@ -1,7 +1,7 @@
-carpeta = "ZZ-0006"
+carpeta = "ZZ-0007"
 
 files = list.files(path = paste0("~/buckets/b1/expw/", carpeta, "/"), pattern = "*.csv", full.names = TRUE)
-files = files[grep("ZZ-0006_", files)]
+files = files[grep(paste0(carpeta ,"_"), files)]
 
 l1 <- "#!/bin/bash \n"
 l2 <- "source ~/.venv/bin/activate  \n"
@@ -11,10 +11,11 @@ for (i in 1:length(files)) {
   l3 <- paste0( "kaggle competitions submit -c itba-data-mining-2024-a")
   l3 <- paste0( l3, " -f ", files[i] )
   l3 <- paste0( l3,  " -m ",  "\"", carpeta,  " , ",  files[i] , "\"",  "\n")
-  envio = c(envio, paste0(l3))
+  l4 = paste0("sleep ", sample(1:5, 1), "\n")
+  envio = c(envio, l3, l4)
 }
-l4 <- "deactivate \n"
-sh = c(l1, l2, envio, l4)
+l5 <- "deactivate \n"
+sh = c(l1, l2, envio, l5)
 cat( sh , file = "subirJuan.sh" )
 Sys.chmod( "subirJuan.sh", mode = "744", use_umask = TRUE)
 
