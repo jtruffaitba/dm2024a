bf20ec03bf5e9744f9d2e6135f6f0ff304601f24
Juan Manuel Truffa <jtruffa@itba.edu.ar>
2024-06-06 01:38:30 +0000

    509 para var2 modificaciones de vuelta

--- src/workflow-01/509_workflow_base_var2.r
+++ src/workflow-01/509_workflow_base_var2.r
@@ -386,7 +386,7 @@ wf_sept <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
   DT_incorporar_dataset_competencia2024()
-  CA_catastrophe_base( metodo="ninguno")
+  CA_catastrophe_base( metodo="MachineLearning")
   FEintra_base()
   DR_drifting_base(metodo="rank_cero_fijo")
   FEhist_base()
